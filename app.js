98267dec8d169f7971f7fb5455464be9c36b25cf
DOODpls <floyd_santos64@yahoo.com>
2019-10-24 20:30:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ function render(){
 
   
   weeksumm.textContent = x.daily.summary
-  preload.style.display = 'none'
+  
   
 
   skycons.set("icon0", Skycons = x.currently.icon);
@@ -203,7 +203,7 @@ function render(){
   for(y=0;y<8;y++){
     skycons.set('icons'+(y+1), Skycons = x.daily.data[y].icon) //////seperate loop for icons
   }
-
+  preload.style.display = 'none'
 
 
 //--------------------------------------------HOURLY-------------------------------------------------//     
